89d6977a351d18318dc318d795e3db9c5b27ff8d
jonasbarzee <jonascbarzee@gmail.com>
2026-03-01 22:05:16 -0700

    update config to exit terminal mode with double escape keypress

--- init.lua
+++ init.lua
@@ -68,6 +68,8 @@ vim.keymap.set('n', '<leader>i', ':e $MYVIMRC<CR>')
 
 vim.keymap.set('n', '<Esc>', ':nohlsearch<CR>')
 
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
